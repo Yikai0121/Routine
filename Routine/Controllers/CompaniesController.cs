cd1d09cb5c4eb4df651b1e1e83ecfe78b5978595
Yikai0121 <k2lairrol@gmail.com>
2020-08-04 17:23:27 +0800

    Initial commit

---
+++ Routine/Controllers/CompaniesController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Routine.APi.Services;
+using Routine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+/*
+ * HTTP请求：
+ * GET      - 查询
+ * POST     - 创建/添加
+ * PATCH    - 局部修改/更新
+ * PUT      - 如果存在就替换，不存在则创建
+ * DELETE   - 移除/删除
+ */
+
+/*
+ * 返回状态码：
+ * 
+ * 2xx 成功
+ * 200 - OK 請求成功
+ * 201 - Created 請求成功建了資源
+ * 204 - No Content 請求成功，但是不應該返回任何東西，例如刪除操作
+ * 
+ * 4xx 客户端錯误
+ * 400 - Bad Request API發送到伺服器的請求是有錯误的
+ * 401 - Unauthorized 没有提供授權信息或者提供的授權信息不正确
+ * 403 - Forbidden 身份認證已經成功，但是已認證的用戶卻無法訪問请求的资源
+ * 404 - Not Found 請求的資源不存在
+ * 405 - Method Not Allowed 常是發送請求到資源的时候，使用了不被支持的HTTP方法
+ * 406 - Not Acceptable API請求的表述格式不被Web API所支持，并且API不
+ *       會提供默認的表述格式
+ * 409 - Conflict 請求伺服器當前狀態衝突（通常指更新資源發生的衝突）
+ * 415 - Unsupported Media Type 與406正好相反，有一些请求必須帶著数据发往伺服器，
+ *       這些數據都属于特定的媒體格式，如果API不支持該媒體类型格式，415就會被返回
+ * 422 - Unprocessable Entity 它是HTTP拓展協議的一部分，它說明伺服器已经懂得了
+ *       Content Type，實體的語法也没有问题，但是伺服器無法處理這個實體数据 
+ * 
+ * 5xx 伺服器錯误
+ * 500 - Internal Server Error 伺服器出現錯誤
+ */
+
+namespace Routine.APi.Controllers
+{
+    /*[ApiController]属性並不是强制要求的，但是它會使發開體驗更好
+     * 它会启用以下行为：
+     * 1.要求使用属性路由（Attribute Routing）
+     * 2.自動HTTP 400響應
+     * 3.推断参数的绑定源
+     * 4.Multipart/form-data请求推断
+     * 5.錯誤狀態代碼的問題詳細信息
+     */
+    [ApiController]
+    [Route("api/companies")] //可用 [Route("api/[controller]")]
+    public class CompaniesController:ControllerBase
+    {
+        private readonly ICompanyRepository _companyRepository;
+
+        public CompaniesController(ICompanyRepository companyRepository)
+        {
+            this._companyRepository = companyRepository ??
+                                        throw new ArgumentNullException(nameof(companyRepository));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCompanies()
+        {
+            var companies = await _companyRepository.GetCompaniesAsync();
+            var companyDtos = new List<CompanyDto>();
+            foreach (var company in companies)
+            {
+                companyDtos.Add(new CompanyDto
+                {
+                    Id = company.Id,
+                    Name = company.Name
+                });
+            }
+            return Ok(companyDtos);  //OK() 返回200
+        }
+
+        [HttpGet("{companyId}")]  //还可用 [Route("{companyId}")]
+        public async Task<IActionResult> GetCompany(Guid companyId)
+        {
+            //較不適合的方法：
+            //var exist = await _companyRepository.CompanyExistsAsync(companyId);
+            //if (!exist)
+            //{
+            //    return NotFound(); //返回404
+            //}
+            //var company = await _companyRepository.GetCompanyAsync(companyId);
+            //return Ok(company);
+            //
+
+            //略有改善的方法：
+            var company = await _companyRepository.GetCompanyAsync(companyId);
+            if (company == null)
+            {
+                return NotFound();  //返回404
+            }
+            return Ok(company);
+        }
+
+    }
+}
---
+++ Routine/Startup.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Routine.APi.Data;
+using Routine.APi.Services;
+
+namespace Routine
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers(setup =>
+            {
+                setup.ReturnHttpNotAcceptable = true; //返回值不是Json格式返回406
+                //setup.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());//加入xml返回格式
+                //setup.OutputFormatters.Insert(0, new XmlDataContractSerializerOutputFormatter()); 預設設定為XML
+            }).AddXmlDataContractSerializerFormatters();//.net core 新增方法input、ouput都會新增
+            services.AddScoped<ICompanyRepository, CompanyRepository>();
+
+            services.AddDbContext<RoutineDbContext>(option =>
+            {
+                option.UseSqlite("Data Source=routine.db");
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
